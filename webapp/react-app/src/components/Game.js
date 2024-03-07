04a54238116b474d100f0f58a649dd0b11608795
joetchek <tomichekitf7@gmail.com>
2024-03-07 16:07:05 -0600

    3-7-24 continued work on Game.js

--- webapp/react-app/src/components/Game.js
+++ webapp/react-app/src/components/Game.js
@@ -3,32 +3,37 @@ import {useState, useEffect } from "react"
 const Game = ({gamePath}) => {
 
     //instead of having setGame to an object, just grab the whole data from the json
-    //grab data -- json.map into new array of objects -- set game to be new mapped array
-    const [game, setGame] = useState({
-        id: null,
-        name: null,
-        rating: null, 
-    })
+    //grab data -- json.map into new array of objects -- set game to be new mapped array --DONE
+    const [gameData, setGameData] = useState([])
 
     //get the game name
     useEffect(() => {
         fetch(gamePath).then(result => result.json()).then(data => {
-            setGame({
-                id: data[1].id,
-                name: data[1].name,
-                rating: data[1].rating
-             })
+            //console.log(data)
+            //creating a JS object here so use the brackets inside the parenthesis
+            const newData = data.map(object => ({
+                id: object.id,
+                name: object.name,
+                rating: object.rating
+            }));
+            console.log(newData)
+            setGameData(newData)
         });
     }, []);
 
-
+    //make the games return a game component for each of these next
     return(
         <div>
-            <p>The game name is {game.name}</p>
-            <p>The game id is {game.id}</p>
-            <p>The game rating is {game.rating}</p>
+            {gameData.map(game => (
+                <p>
+                    <li>
+                        <p>The games id is {game.id}</p>
+                        <p>The games name is {game.name}</p>
+                        <p>The games rating is {game.rating}</p>
+                    </li>
+                </p>
+            ))}
         </div>
-
     );
 }
 
